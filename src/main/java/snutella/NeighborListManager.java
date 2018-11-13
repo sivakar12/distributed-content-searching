f7441f768ee34a6d5fc8f66dfccbf37758ca0c3b
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-11-13 12:09:01 +0530

    Make node manager limit the number of connections

--- src/main/java/snutella/NeighborListManager.java
+++ src/main/java/snutella/NeighborListManager.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NeighborListManager {
+    private final static int MAX_CONNECTIONS = 2;
+
     private List<Neighbor> neighbors;
 
     public NeighborListManager() {
         this.neighbors = new ArrayList<Neighbor>();
+        Runnable refreshConnection = () -> {
+            while (true) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.refreshConnections();
+            }
+        };
+        (new Thread(refreshConnection)).start();
     }
 
     public synchronized void addNeighbor(Neighbor neighbor) {
@@ -21,5 +34,27 @@ public class NeighborListManager {
 
     public List<Neighbor> getNeighbors() {
         return this.neighbors;
+    }
+
+    public long getNumberOfConnections() {
+        return this.neighbors.stream().filter(
+                n -> n.getIsConnected()).count();
+    }
+
+    public void refreshConnections() {
+        long numberOfConnections = getNumberOfConnections();
+        if (numberOfConnections > MAX_CONNECTIONS) {
+            long numberToDisconnect = numberOfConnections - MAX_CONNECTIONS;
+            this.neighbors.stream()
+                    .filter(n -> n.getIsConnected())
+                    .limit(numberToDisconnect)
+                    .forEach(neighbor -> neighbor.setIsConnected(false));
+        } else if (numberOfConnections < MAX_CONNECTIONS) {
+            long numberToConnect = MAX_CONNECTIONS - numberOfConnections;
+            this.neighbors.stream()
+                    .filter(n -> !n.getIsConnected())
+                    .limit(numberToConnect)
+                    .forEach(neighbor -> neighbor.setIsConnected(true));
+        }
     }
 }
